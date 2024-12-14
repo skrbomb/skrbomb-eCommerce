894a85d14b5698f5a122434c7552a53f2e52a93c
skrbomb <clcc620@gmail.com>
2024-12-14 19:05:56 +0800

    Initialize project with  frontend and backend folders

---
+++ backend/src/main/java/com/skrbomb/eCommerce/SkrbombECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.skrbomb.eCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkrbombECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkrbombECommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/skrbomb/eCommerce/SkrbombECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrbomb.eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkrbombECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
